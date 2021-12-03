4670b5c59043c9ad6332c7801b96cdba47078129
Kevin Guedes <kevinguedes@edu.unifor.br>
2021-12-03 15:05:29 -0300

    Handling invalid domain exception when invalid data point is generated

--- Backend/RealTimeCharts.Microservices.DataProvider/Handlers/DataGenerationRequestedEventHandler.cs
+++ Backend/RealTimeCharts.Microservices.DataProvider/Handlers/DataGenerationRequestedEventHandler.cs
@@ -3,6 +3,7 @@ using OperationResult;
 using RealTimeCharts.Infra.Bus.Interfaces;
 using RealTimeCharts.Microservices.DataProvider.Interfaces;
 using RealTimeCharts.Shared.Events;
+using RealTimeCharts.Shared.Exceptions;
 using RealTimeCharts.Shared.Handlers;
 using System;
 using System.Threading;
@@ -28,29 +29,31 @@ namespace RealTimeCharts.Microservices.DataProvider.Handlers
 
         public Task<Result> Handle(DataGenerationRequestedEvent @event, CancellationToken cancellationToken)
         {
-            _logger.LogInformation($"{@event.DataType} data generation started");
-            var optimalSetup = _dataGenerator.GetOptimalSetupFor(@event.DataType);
-
             try
             {
+                _logger.LogInformation($"{@event.DataType} data generation started");
+                var optimalSetup = _dataGenerator.GetOptimalSetupFor(@event.DataType);
+
                 for (double i = optimalSetup.Min; i <= optimalSetup.Max; i += optimalSetup.Step)
                 {
                     var dataPoint = _dataGenerator.GenerateData(i, @event.DataType);
+
                     _logger.LogInformation($"Publishing {@event.DataType} data generated event to dispatcher");
                     _eventBus.Publish(new DataGeneratedEvent(dataPoint, @event.ConnectionId));
+
                     Thread.Sleep(_dataGenerator.GetSleepTimeByGenerationRate(@event.DataGenerationRate));
                 }
+
+                _logger.LogInformation($"{@event.DataType} data successfully generated");
+                _eventBus.Publish(new DataGenerationFinishedEvent(@event.ConnectionId, true));
+                return Result.Success();
             }
-            catch (Exception ex)
+            catch (InvalidDomainException ex)
             {
                 _logger.LogError($"Failed to generate Data: {ex.Message}");
                 _eventBus.Publish(new DataGenerationFinishedEvent(@event.ConnectionId, false));
                 return Result.Success();
             }
-
-            _logger.LogInformation($"{@event.DataType} data successfully generated");
-            _eventBus.Publish(new DataGenerationFinishedEvent(@event.ConnectionId, true));
-            return Result.Success();
         }
     }
 }
