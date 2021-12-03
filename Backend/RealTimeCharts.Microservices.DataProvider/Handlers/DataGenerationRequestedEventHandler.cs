04f1d81ed633bc29ed1424e9b361c57d01474f3e
Kevin Guedes <kevinguedes@edu.unifor.br>
2021-12-03 10:34:26 -0300

    Refactory on data point model

--- Backend/RealTimeCharts.Microservices.DataProvider/Handlers/DataGenerationRequestedEventHandler.cs
+++ Backend/RealTimeCharts.Microservices.DataProvider/Handlers/DataGenerationRequestedEventHandler.cs
@@ -4,6 +4,7 @@ using RealTimeCharts.Infra.Bus.Interfaces;
 using RealTimeCharts.Microservices.DataProvider.Interfaces;
 using RealTimeCharts.Shared.Events;
 using RealTimeCharts.Shared.Handlers;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,8 +17,8 @@ namespace RealTimeCharts.Microservices.DataProvider.Handlers
         private readonly IDataGenerator _dataGenerator;
 
         public DataGenerationRequestedEventHandler(
-            ILogger<DataGenerationRequestedEventHandler> logger, 
-            IEventBus eventBus, 
+            ILogger<DataGenerationRequestedEventHandler> logger,
+            IEventBus eventBus,
             IDataGenerator dataGenerator)
         {
             _eventBus = eventBus;
@@ -30,21 +31,21 @@ namespace RealTimeCharts.Microservices.DataProvider.Handlers
             _logger.LogInformation($"{@event.DataType} data generation started");
             var optimalSetup = _dataGenerator.GetOptimalSetupFor(@event.DataType);
 
-            for (double i = optimalSetup.Min; i <= optimalSetup.Max; i += optimalSetup.Step)
+            try
             {
-                var dataPoint = _dataGenerator.GenerateData(i, @event.DataType);
-
-                if (!dataPoint.IsValid)
+                for (double i = optimalSetup.Min; i <= optimalSetup.Max; i += optimalSetup.Step)
                 {
-                    _logger.LogError($"Failed to generate Data: Invalid data generated");
-                    _eventBus.Publish(new DataGenerationFinishedEvent(@event.ConnectionId, false));
-                    return Result.Success();
+                    var dataPoint = _dataGenerator.GenerateData(i, @event.DataType);
+                    _logger.LogInformation($"Publishing {@event.DataType} data generated event to dispatcher");
+                    _eventBus.Publish(new DataGeneratedEvent(dataPoint, @event.ConnectionId));
+                    Thread.Sleep(_dataGenerator.GetSleepTimeByGenerationRate(@event.DataGenerationRate));
                 }
-
-                _logger.LogInformation($"Publishing {@event.DataType} data generated event to dispatcher");
-                _eventBus.Publish(new DataGeneratedEvent(dataPoint, @event.ConnectionId));
-
-                Thread.Sleep(_dataGenerator.GetSleepTimeByGenerationRate(@event.DataGenerationRate));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to generate Data: {ex.Message}");
+                _eventBus.Publish(new DataGenerationFinishedEvent(@event.ConnectionId, false));
+                return Result.Success();
             }
 
             _logger.LogInformation($"{@event.DataType} data successfully generated");
--- Backend/RealTimeCharts.Shared/Models/DataPoint.cs
+++ Backend/RealTimeCharts.Shared/Models/DataPoint.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using RealTimeCharts.Shared.Exceptions;
 using System;
 
 namespace RealTimeCharts.Shared.Models
@@ -7,15 +8,17 @@ namespace RealTimeCharts.Shared.Models
     {
         public DataPoint(double name, double value)
         {
+            ValidateDomain(name, value);
+
             Name = Math.Round(name, 3);
             Value = Math.Round(value, 3);
         }
 
         public double Name { get; }
         public double Value { get; }
-        [JsonIgnore]
-        public bool IsValid { get => !Double.IsNaN(Name) && !Double.IsNaN(Value); }
-
         public override string ToString() => $"({Name}, {Value})";
+
+        private void ValidateDomain(double name, double value)
+            => InvalidDomainException.When(Double.IsNaN(name) || Double.IsNaN(value), "Invalid domain property");
     }
 }
